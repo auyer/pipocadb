8e717ba9ec06f09e32634f1719c506c402724a78
Rafael Passos <rafa_auyer@icloud.com>
2017-06-27 22:28:09 +0000

    completed user universe integration

--- manteiga.py
+++ manteiga.py
@@ -10,7 +10,8 @@ import flask_admin as admin
 from flask_admin.contrib import sqla
 from flask_admin.contrib.sqla import filters
 from sqlalchemy import CheckConstraint
-
+from sqlalchemy.orm.session import object_session
+from datetime import datetime
 app = Flask(__name__)
 
 # Create dummy secrey key so we can use sessions
@@ -54,9 +55,18 @@ class person(db.Model):
 	country = db.Column(db.ForeignKey('country.idcountry'),nullable=False)
 	art_name = db.Column(db.String(15),nullable=True)
 	
-	first_name = db.relationship('fname')
-	last_name = db.relationship('lname')
-	country_name= db.relationship('country') 
+	First_Name = db.relationship('fname')
+	Last_Name = db.relationship('lname')
+	Country_Name= db.relationship('country') 
+
+	def __repr__(self):
+		return self.art_name
+	
+	def get_aname(idperson):
+		session = object_session(person)
+		a= session.querry(art_name).filter(person.idperson==idperson)
+		print("\n\n\n",a,"\n\n\n")
+		return a	
 
 class insert_person(db.Model):
 	idperson = db.Column(db.Integer, primary_key=True)
@@ -73,6 +83,10 @@ class content_name(db.Model):
 	
 	def __repr__(self):
 		return self.name
+	
+	def get_name(self,idn):
+		session = object_session(self)
+		return session.querry(name).filter(content_name.idcname==idn)		
 
 class cast_job(db.Model):
 	idjob = db.Column(db.Integer, primary_key=True)
@@ -81,26 +95,32 @@ class cast_job(db.Model):
 	
 	def __repr__(self):
 		return self.jobname
+
 class content(db.Model):
 	idcontent = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.ForeignKey(content_name.idcname), unique=True,nullable=False)
 	country = db.Column(db.ForeignKey('country.idcountry'),nullable=False)
 	released = db.Column(db.Date(),nullable=False)
 
-	Content_name = db.relationship('content_name')	
-	country_name = db.relationship('country')	
+	Content_Name = db.relationship('content_name')	
+	Country_Name = db.relationship('country')	
 	
 
 	def __repr__(self):
-		return self.name
+		#return content_name.get_name( self ,self.idcontent)
+		return str(self.idcontent)
 
 class cast_member(db.Model):
 	idperson = db.Column(db.ForeignKey(person.idperson), primary_key=True)
 	idcontent = db.Column(db.ForeignKey(content.idcontent),primary_key=True)
 	idjob = db.Column(db.ForeignKey(cast_job.idjob),primary_key=True)
+
+	Person = db.relationship(person)
+	Content_ID = db.relationship(content)
+	Job = db.relationship(cast_job)
 	
 	def __repr__(self):
-		return self.idperson
+		return person.get_aname(self.idperson)
 	#Uma pessoa pode ter mais de uma função em um memso filme ( ex: Produtor e DIretor)
 
 
@@ -113,28 +133,35 @@ class user(db.Model):
 	lname = db.Column(db.ForeignKey('lname.idlname'),nullable=False )
 	born = db.Column(db.Date(), nullable=False)
 	country = db.Column(db.ForeignKey('country.idcountry'),nullable=False)
-	email = db.Column(EmailType,nullable=False)
+	#email = db.Column(EmailType,nullable=False)
+	email = db.Column(db.String(45), unique=True)
 	ative_until = db.Column(db.DateTime())
-	
-	#def __repr__(self):
-		#return self.
+	CheckConstraint("REGEXP_LIKE(email,'^[a-zA-Z][a-zA-Z0-9_\.\-]+@([a-zA-Z0-9-]{2,}\.)+([a-zA-Z]{2,4}|[a-zA-Z]{2}\.[a-zA-Z]{2})$')", name='emailcheck')	
+	First_Name = db.relationship('fname')
+	Last_Name = db.relationship('lname')
+	Country_Name= db.relationship('country')
+
+	def __repr__(self):
+		return str(self.iduser)
 
 class content_viewed(db.Model):
 	user_id = db.Column(db.ForeignKey(user.iduser), primary_key=True)
 	content_id = db.Column(db.ForeignKey(content.idcontent),primary_key=True)
-	datetime = db.Column(db.DateTime(),primary_key=True)
-	rating = db.Column(db.Boolean())
-	datetime = db.Column(db.DateTime(),primary_key=True)
+	datetime = db.Column(db.DateTime(),primary_key=True, default=str(datetime.now()))
 	rating = db.Column(db.Boolean())
 	percentage = db.Column(db.Numeric, nullable=True)
 	CheckConstraint('percentage <100 ', name='percentagecheck')
 
+	Content = db.relationship('content')
+	User = db.relationship('user')
+	
 	
 class search(db.Model):
 	user_id = db.Column(db.ForeignKey(user.iduser), primary_key=True)
 	terms_hash = db.Column(db.String(145) ,primary_key=True)
-	datetime = db.Column(db.DateTime(),nullable=False)
+	datetime = db.Column(db.DateTime(),nullable=False, default=str(datetime.now()))
 
+	User_ID = db.relationship('user') 
 #################
 # Flask views
 @app.route('/')
@@ -150,6 +177,7 @@ admin.add_view(sqla.ModelView(fname, db.session))
 admin.add_view(sqla.ModelView(lname, db.session))
 admin.add_view(sqla.ModelView(country, db.session))
 admin.add_view(sqla.ModelView(person, db.session))
+admin.add_view(sqla.ModelView(insert_person, db.session))
 admin.add_view(sqla.ModelView(content_name, db.session))
 admin.add_view(sqla.ModelView(cast_job, db.session))
 admin.add_view(sqla.ModelView(content, db.session))
